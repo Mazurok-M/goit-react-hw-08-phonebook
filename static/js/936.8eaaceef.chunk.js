a8f8dd359c9493253a5740de9d774f5147e6112f
Mazurok-M <108468316+Mazurok-M@users.noreply.github.com>
2022-11-17 21:56:27 +0000

    Deploying to gh-pages from @ Mazurok-M/goit-react-hw-08-phonebook@4351ac697934d648ba13e2db6803b85ac0528bc4 🚀

---
+++ static/js/936.8eaaceef.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[936],{4936:function(e,n,t){t.r(n),t.d(n,{default:function(){return r}});var o="Home_container__w+u63",_="Home_title__L087v",c=t(184),r=function(){return(0,c.jsx)("div",{className:o,children:(0,c.jsx)("h1",{className:_,children:" \u0412\u0456\u0442\u0430\u0454\u043c\u043e \u043d\u0430 \u043d\u0430\u0448\u043e\u043c\u0443 \u0441\u0435\u0440\u0432\u0456\u0441\u0456"})})}}}]);
+//# sourceMappingURL=936.8eaaceef.chunk.js.map
